7dc8828f3085ae2d7b44f46485538c0b02508df3
elhabib.amazzal <elhabib.amazzal@adevinta.com>
2021-01-06 19:07:04 +0100

    add packages

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import 'fontsource-roboto';
+
 
 ReactDOM.render(
   <React.StrictMode>
